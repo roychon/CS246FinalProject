552540fa08395ed18aff66b327e61c76472e4e95
rchon <rchon@uwaterloo.ca>
2023-11-26 13:29:29 -0500

    start implementation for Link

---
+++ link.cc
@@ -0,0 +1,17 @@
+#include "link.h"
+
+string Link::getType() {
+    return type;
+}
+
+int Link::getStrength() {
+    return strength;
+}
+
+int Link::getX() {
+    return x;
+}
+
+int Link::getY() {
+    return y;
+}
--- link.h
+++ link.h
@@ -8,8 +8,8 @@ class Link {
     int strength; // strength of link
     string type; // type of link
     bool isRevealed;
-    int row;
-    int col;
+    int x; // row
+    int y; // col
     Player *player; // ptr to player who owns this link
     int moveFactor; // controls distance of movement for link on game board
 
